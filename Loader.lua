bf215bafb70f6f8117280eb7cd48f695aed35b56
shadeflux <shadefluxpl@gmail.com>
2026-03-07 15:47:41 +0100

    Initialize Quantum X Loader and check for loading

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/shadeflux/QuantumX/main/Loader.lua"))()
+-- Quantum X Loader
+if getgenv().QuantumXLoaded then return end
+getgenv().QuantumXLoaded = true
+
+if not game:IsLoaded() then game.Loaded:Wait() end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shadeflux/QuantumX/main/Hub.lua", true))()
