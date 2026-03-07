aa95989e2ac022856da2fe0500121d5594fae838
shadeflux <shadefluxpl@gmail.com>
2026-03-07 17:00:30 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/shadeflux/QuantumX/main/Hub.lua"))()
+-- Quantum X Loader | Unseen. Unpatched. Unstoppable.
+
+if getgenv().QuantumXLoaded then return end
+getgenv().QuantumXLoaded = true
+
+if not game:IsLoaded() then game.Loaded:Wait() end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shadeflux/QuantumX/main/Hub.lua", true))()
