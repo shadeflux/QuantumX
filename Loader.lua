685deb5d94ec68e375bb25538a99701c7ba5df81
shadeflux <shadefluxpl@gmail.com>
2026-03-07 18:07:13 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/shadeflux/QuantumX/main/Hub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shadeflux/QuantumX/main/Hub.lua?v=" .. math.random(100000,999999)))()
