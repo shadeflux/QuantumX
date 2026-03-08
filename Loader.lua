5153e91e9461ecdee4eed5d340dce27e75f8f739
shadeflux <shadefluxpl@gmail.com>
2026-03-08 11:02:08 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/shadeflux/QuantumX/main/Hub.lua"))()
+loadstring(game:HttpGet("https://ghproxy.com/https://raw.githubusercontent.com/shadeflux/QuantumX/main/Hub.lua"))()
